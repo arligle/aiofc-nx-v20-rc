f1a233756e819563f9fa46db1340e084eb205a76
arligle <0774543002@163.com>
2024-10-23 03:28:53 +0800

    feat(libs): created @aiokit/string-utils

---
+++ libs/string-utils/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'string-utils',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  setupFilesAfterEnv: ['../../jest.setup.js'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/string-utils',
+};
